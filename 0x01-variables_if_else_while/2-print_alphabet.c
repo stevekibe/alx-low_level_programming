a30283a5f7f9461d2d5ae2bcac6eb6bbba8387ab
stevekibe <stevekibe2345@gmail.com>
2023-06-16 07:55:57 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start of programm
+ *
+ * Return: 0 
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a<= 'z' ; a++)
+		putchar(a);
+	for (A = 'A'; A<= 'Z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return(0);
+}
