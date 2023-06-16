420d45be04406ae0c76d564344fe5e04c1a3ddff
stevekibe <stevekibe2345@gmail.com>
2023-06-16 14:24:32 -0700

    tebahpla'

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - start of code
+ *
+ * Return: is always false 0
+ *
+ */
+int main (void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a' ; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
