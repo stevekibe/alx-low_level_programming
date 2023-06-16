560372eaa8f4b6324b063d31809826d256462266
stevekibe <stevekibe2345@gmail.com>
2023-06-16 08:48:23 -0700

    changes

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,15 +3,16 @@
  *
  * main - start of code
  *
- * Return: 0
+ * Return: 0 return false
  *
  */
 int main(void)
 {
 	int a = 0;
 
+
 	for (; a < 10; a++)
-	       printf("%d", a);
+		printf("%d", a);
 	putchar('\n');
 	return (0);
 }	
