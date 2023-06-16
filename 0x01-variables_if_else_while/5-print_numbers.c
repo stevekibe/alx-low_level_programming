68cafacf975334e3f689518e6d24f40259eb4f53
stevekibe <stevekibe2345@gmail.com>
2023-06-16 08:50:52 -0700

    changes

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- *
  * main - start of code
  *
- * Return: 0 return false
+ * Return: return false
  *
  */
 int main(void)
@@ -15,4 +14,4 @@ int main(void)
 		printf("%d", a);
 	putchar('\n');
 	return (0);
-}	
+}
