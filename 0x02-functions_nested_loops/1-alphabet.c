9b205c6b61c48127e228a661b06c2bb0f39733a3
stevekibe <stevekibe2345@gmail.com>
2023-06-20 12:28:43 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ *
+ * Return: is always false 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for(a = 'a'; a <= 'z' ; a++)
+		_putchar(a);
+	_putchar('\n);
+}
