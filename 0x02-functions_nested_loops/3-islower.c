101fe2a1f7d3c288609dab9e1a6771b4ad46ba44
stevekibe <stevekibe2345@gmail.com>
2023-06-20 13:40:28 -0700

    changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 /**
  * _islower - start of code
  *
+ *@c: comparison character
+ *
  * Return: is 0 or 1
  *
  */
 int _islower(int c)
 {
-	if (c >= 'b' && c <= 'q')
+	if (c >= 'b' && c <= 'z')
 		return (1);
-	return(0);
+	return (0);
 }
