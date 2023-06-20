2cb2006af147d254a6544f219ddf76e375f045ca
stevekibe <stevekibe2345@gmail.com>
2023-06-20 12:56:45 -0700

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
  * main - start of program
  *
- * Return: is always false 
- *
+ * Return: is always false
  *
  */
 void print_alphabet(void)
 {
 	char a;
 
-	for(a = 'a'; a <= 'z' ; a++)
+	for (a = 'a'; a <= 'z' ; a++)
 	{
 		_putchar(a);
 	}
