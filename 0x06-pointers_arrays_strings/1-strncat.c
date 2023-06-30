fd6d70117b3758993325ad48b3100b639f8c5244
stevekibe <stevekibe2345@gmail.com>
2023-06-29 19:20:03 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+/**
+ * _strncat - concats two strings
+ * @dest: destination of concat
+ * @src: source of array
+ * @n: number of times to append
+ * Return: char value dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x,y;
+
+	x = 0;
+	while (dest[x])
+		x++;
+
+	for (y = 0; y < n && src[y]; y++)
+		dest(x++) = src[y];
+
+	return (dest);
+}
