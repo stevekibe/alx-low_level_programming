ae90cb6509ad9ba5a997b98a556f8ef057618510
stevekibe <stevekibe2345@gmail.com>
2023-07-03 16:26:02 +0300

    changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,8 @@
 /**
- * _memset -sets the momory of the array
+ * _memset - sets the momory of the array
  * @s: array to set
  * @b: value to set it as
  * @n: number of times to set
- * 
  * Return: value of s
 */
 char *_memset(char *s, char b, unsigned int n)
