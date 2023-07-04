8f690e7123e6588fe63c12bfdc2b9fa0c26e32ea
stevekibe <stevekibe2345@gmail.com>
2023-07-04 22:34:19 +0300

    changes

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_chessboard - board to print
  * @a: prints 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,12 @@
  * Return: value of s
 */
 char *_memset(char *s, char b, unsigned int n);
+/**
+ * print_chessboard - board to print
+ * @a: prints 
+ * Return: nothing
+*/
+void print_chessboard(char (*a)[8]);
 
 
 #endif
