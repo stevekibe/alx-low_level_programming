f7ac3818a1319dc19e2fddb795f33ae33a2230b2
stevekibe <stevekibe2345@gmail.com>
2023-07-04 22:24:50 +0300

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+/**
+ * print_chessboard - board to print
+ * @a: prints 
+ * Return: nothing
+*/
+void print_chessboard(char (*a)[8])
+{
+    int x;
+    int y;
+
+    for(x = 0; x < 8; x++)
+    {
+        for(y = 0; y < 8; y++)
+        {
+
+            _putchar(a[x][y]);
+        }
+        _putchar('\n');
+    }
+}
