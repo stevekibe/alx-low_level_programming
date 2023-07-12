a6b81eef72829976aa7004237812ab4e97ccbc69
stevekibe <stevekibe2345@gmail.com>
2023-07-12 10:28:05 +0300

    serch for string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+/**
+ * _strpbrk - finds string
+ * @s: string to find stuff
+ * @accept: accepting arrays
+ *
+ * Return: returns new value
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+	int y;
+
+	for (x = 0; s[x] != '\0'; y++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				return (s + x);
+			}
+		}
+	}
+	return (0);
+}
