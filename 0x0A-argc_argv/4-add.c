49aec982bdc6b272c19d43bcb67c5a385a663076
stevekibe <stevekibe2345@gmail.com>
2023-07-12 19:38:48 +0300

    changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
  * main - main functiom.
  * @argc: counts arguments.
  * @argv: array of arguments.
  * Return:Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int b, a, add = 0;
 
-	for (i = 1; i < argc; i++)
+	for (b = 1; b < argc; b++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (a = 0; argv[b][a] != '\0'; a++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (!isdigit(argv[b][a]))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		add += atoi(argv[b]);
 	}
 	printf("%d\n", add);
 	return (0);
