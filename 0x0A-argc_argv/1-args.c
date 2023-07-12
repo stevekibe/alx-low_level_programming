7e05882cbb4cdac972971d606595a9e717523509
stevekibe <stevekibe2345@gmail.com>
2023-07-12 19:05:27 +0300

    changes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#define UNUSED(x) void(x)
+#define UNUSED(x) (void)(x)
 /**
  * main -main function
  * @argc: count arguments
  * @argvn: array of argc
  * Return: always 0
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     UNUSED(argv);
     printf("%d\n", argc -1);
