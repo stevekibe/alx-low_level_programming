a6d4a9400c959fa24e6c636f3f6c8c1b703cb2ac
stevekibe <stevekibe2345@gmail.com>
2023-07-13 05:34:55 +0300

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,11 +4,11 @@
 /**
  * print_grid - prints a grid of integers
  * @grid: the address of the two dimensional grid
- * @width: width of the grid
+ *
  * @height: height of the grid
  *
  */
-void print_grid(int **grid, int width, int height)
+void free_grid(int **grid, int height)
 {
     int i;
 
