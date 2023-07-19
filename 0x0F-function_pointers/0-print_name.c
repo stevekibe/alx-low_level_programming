19efc0f91f7446b45c3c7f79f2cd8c5b47721b82
stevekibe <stevekibe2345@gmail.com>
2023-07-20 02:52:46 +0300

    changes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,9 @@
  * print_name - prints the name
  * @name: character string
  * @f: function pointer
- * Return: nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-        f(name);
-    
+	if (name != NULL && f != NULL)
+		f(name);
 }
