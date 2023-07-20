fe1d306856c8cd3246fd99c2f25c9f6dfb9a2318
stevekibe <stevekibe2345@gmail.com>
2023-07-20 03:08:00 +0300

    changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,19 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 /**
  * array_iterator - calls a function to act on array elements
  * @array: Name of the array in which the function acts on.
  * @size: size of the passed array to function
  * @action: the function that acts on the array elements
+ * Return:there is  no return cause we've declared VOID
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-   unsigned int i;
+	unsigned int i;
 
 	if (!array || !action)
 		return;
 
 	for (i = 0; i < size; i++)
-		action(array[i]);         
+		action(array[i]);
 }
