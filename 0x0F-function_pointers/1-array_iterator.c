d997d2fd2099f2df896f7f7376e4d2fdf7d042e0
stevekibe <stevekibe2345@gmail.com>
2023-07-20 03:05:27 +0300

    changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
     if (array && size > 0 && action)
     {
 		for (x = 0; x < size; x++)
+        {
 			action(array[x]);
+        }
     }
             
 }
