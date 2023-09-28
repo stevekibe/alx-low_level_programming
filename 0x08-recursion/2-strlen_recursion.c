e824ee997fcf1c32bc0813468ddc1ce1220d3e6e
stevekibe <stevekibe2345@gmail.com>
2023-09-28 03:56:14 +0300

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _int_strlen_recursion - recurssion string length
+ * @s: character to recurse
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int z = 0;
+
+	if (*s)
+	{
+		z = _strlen_recursion(s + 1);
+		return (z += 1);
+	}
+	return (0);
+}
